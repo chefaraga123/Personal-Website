9dc7f57d22c29c4dd11fbca1829ceb3ef6cb2257
James <jamesoleary32@gmail.com>
2025-02-16 16:46:13 +0000

    updates box

--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -8,6 +8,11 @@ const Home = () => {
             <h1>Welcome to My Website</h1>
             <p>I am James, Co-Founder of Footium and have been working at the intersection of Web3 and Gaming since Summer 2020. I like building things for fun.  </p>
             <Navigation />
+            <section className={styles.recentUpdates}>
+                <h2>Updates</h2>
+                <p>16/02/2025: <b>Notes:</b> <a href="/notes/ContractsforDifference">Contracts for Difference</a>, <a href="/notes/EnergyMarketReform">Energy Market Reform</a> which relate to UK energy policy</p>
+                <p>06/02/2025: <b>Article:</b> <a href="/Marketplace-of-Ideas">Marketplace of Ideas</a> which is about the divergent epistemic committments of science and efficient markets</p>
+            </section>
         </div>
     );
 };
